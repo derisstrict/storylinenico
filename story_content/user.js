febb35f05f25abe870977e552a9eaf735da71986
derisstrict <94850683+derisstrict@users.noreply.github.com>
2024-01-10 17:36:22 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "69iJMjz2Ehf":
+        Script1();
+        break;
+      case "5igVkqp885r":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  audio.play();
+}
+
+function Script2()
+{
+  toggleMute();
+}
+
